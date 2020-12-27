9c8d61294d6e3ce062c4fcad590ed8981d66523a
changmink <changmin043@gmail.com>
2020-12-27 17:47:36 +0900

    [NoIssue] Ajax로 내용 가져오기

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 class Blog{
     constructor(args) {
         console.log('Blog is started!')
+        const dataURL = "src/data/data.json"
+        this.setInitData(dataURL);
+    }
+
+    setInitData(dataURL) {
+        this.getData(dataURL);
+    }
+
+    getData(dataURL) {
+        const oReq = new XMLHttpRequest();
+        oReq.addEventListener("load", ()=>{
+            const list = JSON.parse(oReq.responseText).body
+            console.log(list)
+            list.forEach((v) => {
+               console.log(v.title) 
+            });
+        })
+
+        oReq.open('GET', dataURL);
+        oReq.send();
     }
 }
 
